ccfcac15fa1f4e5b1825cb7daf318c3a2cb0104f
sorgazb <sorgazb@gmail.com>
2025-03-26 10:57:21 +0100

    Rutas en Go Gin

--- main.go
+++ main.go
@@ -1,18 +1,14 @@
 package main
 
 import (
+	"Gin/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
-		//	c.String(200, "Hola mundo")
-		c.JSON(200, gin.H{
-			"message": "Hola mundo",
-		})
-	})
+	routes.SetupRoutes(r)
 
 	r.Run(":8080")
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,25 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func SetupRoutes(r *gin.Engine) {
+	r.GET("/", func(c *gin.Context) {
+		//	c.String(200, "Hola mundo")
+
+		/*
+			c.JSON(200, gin.H{
+				"message": "Hola mundo",
+			})
+		*/
+
+		c.String(http.StatusOK, "Hola mundo")
+	})
+
+	r.GET("/saludo/:nombre", func(c *gin.Context) {
+		nombre := c.Param("nombre")
+		c.String(http.StatusOK, "Hola, %s", nombre)
+	})
+}
