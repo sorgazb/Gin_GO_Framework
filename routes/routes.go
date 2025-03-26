943573c2e602e8104860ab238ee8f6af5ee0689c
sorgazb <sorgazb@gmail.com>
2025-03-26 12:16:00 +0100

    Manejo de Solicitudes HTTP

--- routes/routes.go
+++ routes/routes.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+type Usuario struct {
+	Nombre string `json:"nombre"`
+	Email  string `json:"email"`
+}
+
+var usuarios []Usuario
+
 func SetupRoutes(r *gin.Engine) {
 	r.GET("/", func(c *gin.Context) {
 		//	c.String(200, "Hola mundo")
@@ -21,5 +28,21 @@ func SetupRoutes(r *gin.Engine) {
 	r.GET("/saludo/:nombre", func(c *gin.Context) {
 		nombre := c.Param("nombre")
 		c.String(http.StatusOK, "Hola, %s", nombre)
+	})
+
+	r.POST("/usuarios", func(c *gin.Context) {
+		var nuevoUsuario Usuario
+		if err := c.BindJSON(&nuevoUsuario); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Error al decodificar el JSON"})
+			return
+		}
+
+		if nuevoUsuario.Nombre == "" || nuevoUsuario.Email == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Nombre y correo son campos requeridos"})
+		}
+
+		usuarios = append(usuarios, nuevoUsuario)
+
+		c.JSON(http.StatusOK, gin.H{"message": "Usuario Registrado", "datos": usuarios})
 	})
 }
